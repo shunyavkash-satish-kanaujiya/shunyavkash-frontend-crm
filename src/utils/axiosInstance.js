3c1fefadc20b14632ddd84e15193683cb3a36cd9
shunyavkash-vivek-sojitra <vivek.sojitra@shunyavkash.com>
2025-05-02 09:49:40 +0530

    Maintain Files

--- src/utils/axiosInstance.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-export const instance = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_API_URL, // Backend URL
-  timeout: 10000, // 10 seconds timeout
-  //   withCredentials: true, // If you are using cookies, auth sessions
-});
