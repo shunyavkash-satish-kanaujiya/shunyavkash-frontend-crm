4516432ca9c4c6e068c29d11d656a0fe6668e42c
shunyavkash-satish-kanaujiya <satish.kanaujiya@shunyavkash.com>
2025-05-06 17:11:52 +0530

    Integrate functionality to get only uninvoiced timesheet in invoice form

--- src/components/tables/InvoiceTable.jsx
+++ src/components/tables/InvoiceTable.jsx
@@ -84,8 +84,9 @@ export const InvoiceListPage = ({
 
   if (loading)
     return (
-      <div className="flex justify-center items-center h-64">
-        <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-indigo-500"></div>
+      <div className="flex justify-center items-center flex-col h-64">
+        <div className="animate-spin rounded-full h-12 w-12 border-6 border-t-2 border-b-2 border-indigo-500"></div>
+        <p className="mt-2 text-gray-500">Loading invoices...</p>
       </div>
     );
 
